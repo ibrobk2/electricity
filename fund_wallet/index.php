12c6fc357c132cb895892d70a46e7efd4c03f605
Ibrahim Yusuf <ibrobk@gmail.com>
2023-06-22 16:31:59 +0100

    Adding monnify Javascript Integration but not working.

--- fund_wallet/index.php
+++ fund_wallet/index.php
@@ -9,7 +9,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $amount = $_POST['amount'];
     $username = $user['username'];
     $wallet = $user['wallet'];
-
+    $email = $user['email'];
+    $customer_name = $user['full_name'];
+}
+/*
     // Validate the amount (you can add more validation as per your requirements)
     if (!is_numeric($amount) || $amount <= 0) {
         $error = 'Invalid amount entered.';
@@ -35,6 +38,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 }
+
+*/
 ?>
 
 <!DOCTYPE html>
@@ -63,13 +68,79 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     ?>
 
     <form method="POST" action="index.php">
+        <input type="hidden" value="<?=$customer_name; ?>" id="customer_name">
+        <input type="hidden" value="<?=$email; ?>" id="email">
         <label for="amount">Enter Amount:</label>
         <input type="number" name="amount" id="amount" step="0.01" required>
-        <button type="submit">Fund Wallet</button>
+        <button type="submit" onclick="payWithMonnify()">Fund Wallet</button>
     </form>
     </div>
     <footer style="position: absolute; bottom: 10px;margin-left:450px;">
 		<p>&copy; <?php  echo date("Y"); ?>  Department of Computing and Artificial Intelligence, Federal University Dutsinma .</p>
 	</footer>
+
+
+
+
+<!-- Monnify payment Integration Section -->
+<!-- <html>
+<head> -->
+    <script type="text/javascript" src="https://sdk.monnify.com/plugin/monnify.js"></script>
+    <script>
+        function payWithMonnify() {
+            var amount = document.getElementById('amount').value;
+            var customer_name = document.getElementById('customer_name').value;
+            var customer_email = document.getElementById('email').value;
+            MonnifySDK.initialize({
+                amount: amount,
+                currency: "NGN",
+                reference: new String((new Date()).getTime()),
+                customerFullName: customer_name,
+                customerEmail: customer_email,
+                apiKey: "MK_TEST_5V3BS2KWQS",
+                contractCode: "9709811149",
+                paymentDescription: "Wallet Funding",
+                metadata: {
+                    "name": name,
+                    // "age": 45
+                },
+                incomeSplitConfig: [{
+                    "subAccountCode": "MFY_SUB_342113621921",
+                    "feePercentage": 50,
+                    "splitAmount": 1900,
+                    "feeBearer": true
+                }, {
+                    "subAccountCode": "MFY_SUB_342113621922",
+                    "feePercentage": 50,
+                    "splitAmount": 2100,
+                    "feeBearer": true
+                }],
+
+                onLoadStart: () => {
+                    console.log("loading has started");
+                },
+                onLoadComplete: () => {
+                    console.log("SDK is UP");
+                },
+
+                onComplete: function(response) {
+                    //Implement what happens when the transaction is completed.
+                    console.log(response);
+                },
+                onClose: function(data) {
+                    //Implement what should happen when the modal is closed here
+                    console.log(data);
+                }
+            });
+        }
+    </script>
+<!-- </head> -->
+
+<!-- <body>
+    <div>
+        <button type="button" onclick="payWithMonnify()">Pay With Monnify</button>
+    </div>
+</body> -->
 </body>
+
 </html>
